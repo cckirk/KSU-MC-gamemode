8e4da2313bd57d489ed3ebff24e1cecdb756b3af
BuildTools <unconfigured@null.spigotmc.org>
2024-09-11 04:44:52 -0400

    Speedrun Command Added

---
+++ src/main/java/edu/Kennesaw/EsportsMC/ComponentHelper.java
@@ -0,0 +1,31 @@
+package edu.Kennesaw.EsportsMC;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
+
+public class ComponentHelper {
+
+    static MiniMessage mm = MiniMessage.miniMessage();
+
+    public static Component replaceComponent(Component component, String regex, String replacement) {
+        return component.replaceText(builder -> builder.matchLiteral(regex).replacement(replacement));
+    }
+
+    public static Component mmStringToComponent(String text) {
+        return mm.deserialize(text);
+    }
+
+    public static String componentToMM(Component component) {
+        return mm.serialize(component);
+    }
+
+    public static Component jsonToComponent(String json) {
+        return JSONComponentSerializer.json().deserializeOrNull(json);
+    }
+
+    public static String componentToJson(Component component) {
+        return JSONComponentSerializer.json().serializeOrNull(component);
+    }
+
+}
